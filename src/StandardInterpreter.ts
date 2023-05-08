61d1f326fab8d25cbaa95b10117dd9bf3952afbc
Ca5e <roossienkees@gmail.com>
2023-05-08 11:30:17 +0200

    Shorthand notation to set matches to correct values

--- src/StandardInterpreter.ts
+++ src/StandardInterpreter.ts
@@ -14,40 +14,11 @@ export class StandardInterpreter implements Interpreter {
       interpret(match: RegExpMatchArray): Map<string, string> {
             var termProperties: Map<string, string> = new Map();
             if (match.groups != undefined) {
-                  if (match.groups.showtext != undefined && match.groups.showtext != "") {
-                        termProperties.set("showtext", match.groups.showtext);
-                  }
-                  if (match.groups.term != undefined && match.groups.term != "") {
-                        termProperties.set("term", match.groups.term);
-                  } else {
-                        // term is a text that identifies a knowledge artifact, and is specified in the curated text that documents that artifact (in a specific version of the terminology of a specific scope). It will be matched against the term fields of MRG entries in the MRG that documents said terminology. If omitted, it is generated as follows (assuming the MRG to be used has already been identified):
-
-                        // - set term:=showtext;
-                        // - convert every character in the (regex) range [A-Z] to lower-case;
-                        // - convert every sequence of characters [^A-Za-z_-]+ to (a single) - character;
-                        // - if the resulting term matches an element in the list of texts in the formphrases field of an MRG entry, then replace term with 
-                        //   the contents of the term-field of that same MRG entry.
-                        var showtext = termProperties.get("showtext");
-                        if (showtext != undefined) {
-                              var term = showtext.toLowerCase().replace(/[^A-Za-z_-]+/g, "-");
-                              termProperties.set("term", term);
-                        }
-                  }
-                  if (match.groups.trait != undefined && match.groups.trait != "") {
-                        termProperties.set("trait", match.groups.trait);
-                  } else {
-                        termProperties.set("trait", "default");
-                  }
-                  if (match.groups.scopetag != undefined && match.groups.scopetag != "") {
-                        termProperties.set("scopetag", match.groups.scopetag);
-                  } else {
-                        termProperties.set("scopetag", "default");
-                  }
-                  if (match.groups.vsntag != undefined && match.groups.vsntag != "") {
-                        termProperties.set("vsntag", match.groups.vsntag);
-                  } else {
-                        termProperties.set("vsntag", "latest");
-                  }
+                  termProperties.set("showtext", match.groups.showtext);
+                  termProperties.set("term", match.groups.term || match.groups.showtext.toLowerCase().replace(/[^A-Za-z_-]+/g, "-"));
+                  termProperties.set("trait", match.groups.trait || "default");
+                  termProperties.set("scopetag", match.groups.scopetag || "default");
+                  termProperties.set("vsntag", match.groups.vsntag || "latest");
                   this.log.trace(`Interpreted term: ${termProperties.get("term")}`);
             }
 
