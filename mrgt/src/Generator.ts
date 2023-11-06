e0088691e2113c7a825f5bdee82fa148f4c2b822
Ca5e <roossienkees@gmail.com>
2023-11-06 15:06:34 +0100

    Move writeFile() to 'Handler.ts'

--- mrgt/src/Generator.ts
+++ mrgt/src/Generator.ts
@@ -1,8 +1,8 @@
 import { saf } from "./Run.js"
 import { log } from "./Report.js"
 import { Entry, MRG, TuC, Version } from "./Interpreter.js"
+import { writeFile } from "./Handler.js"
 
-import fs = require("fs")
 import path = require("path")
 import yaml = require("js-yaml")
 
@@ -153,34 +153,5 @@ export class Generator {
       writeFile(altmrgURL, yaml.dump(mrgFile, { forceQuotes: true }))
       log.trace(`\t\t'${path.basename(altmrgURL)}' (altvsn)`)
     })
-  }
-}
-
-/**
- * Creates directory tree and writes data to a file.
- * @param fullPath - The full file path.
- * @param data - The data to write.
- * @param force - Whether to overwrite existing files.
- */
-export function writeFile(fullPath: string, data: string, force: boolean = true) {
-  const dirPath = path.dirname(fullPath)
-  const file = path.basename(fullPath)
-  // Check if the directory path doesn't exist
-  if (!fs.existsSync(dirPath)) {
-    // Create the directory and any necessary parent directories recursively
-    try {
-      fs.mkdirSync(dirPath, { recursive: true })
-    } catch (err) {
-      log.error(`\tE007 Error creating directory '${dirPath}':`, err)
-      return // Stop further execution if directory creation failed
-    }
-  } else if (!force && fs.existsSync(path.join(dirPath, file))) {
-    return // Stop further execution if force is not enabled and file exists
-  }
-
-  try {
-    fs.writeFileSync(path.join(dirPath, file), data)
-  } catch (err) {
-    log.error(`\tE008 Error writing file '${path.join(dirPath, file)}':`, err)
   }
 }
---
+++ mrgt/src/Handler.ts
@@ -0,0 +1,33 @@
+import { log } from "./Report.js"
+
+import path = require("path")
+import fs = require("fs")
+
+/**
+ * Creates directory tree and writes data to a file.
+ * @param fullPath - The full file path.
+ * @param data - The data to write.
+ * @param force - Whether to overwrite existing files.
+ */
+export function writeFile(fullPath: string, data: string, force: boolean = true) {
+  const dirPath = path.dirname(fullPath)
+  const file = path.basename(fullPath)
+  // Check if the directory path doesn't exist
+  if (!fs.existsSync(dirPath)) {
+    // Create the directory and any necessary parent directories recursively
+    try {
+      fs.mkdirSync(dirPath, { recursive: true })
+    } catch (err) {
+      log.error(`\tE007 Error creating directory '${dirPath}':`, err)
+      return // Stop further execution if directory creation failed
+    }
+  } else if (!force && fs.existsSync(path.join(dirPath, file))) {
+    return // Stop further execution if force is not enabled and file exists
+  }
+
+  try {
+    fs.writeFileSync(path.join(dirPath, file), data)
+  } catch (err) {
+    log.error(`\tE008 Error writing file '${path.join(dirPath, file)}':`, err)
+  }
+}
