a916b6464b889ebaa05ac6805f39f0f9a178aa28
Ca5e <roossienkees@gmail.com>
2023-11-12 17:46:44 +0100

    Add initial ESLint env and test script

---
+++ __tests__/integration.test.js
@@ -0,0 +1,31 @@
+// import { spawn } from "child_process"
+// import path from "path"
+// import fs from "fs"
+// import { expect } from "chai"
+
+// const __filename = new URL(import.meta.url).pathname
+// const __dirname = path.dirname(__filename)
+
+describe("TRRT", function () {
+  it("should do something", async function () {
+    // Mock process.argv to simulate command-line arguments
+    process.argv = [
+      // "node",
+      // "your-cli-script.js",
+      // "-c",
+      // "path/to/your/config.yaml",
+      // "-o",
+      // "outputDir",
+      // "-s",
+      // "scopeDir",
+      // "inputPattern"
+    ]
+
+    // Call the main function
+    // await main()
+
+    // Write your assertions based on the expected behavior of your CLI
+    // For example, check if files were generated in the output directory
+    // ...
+  })
+})
