925603cda70ea33c6d6494929ad75c856ec95f7a
Rieks <RieksJ@users.noreply.github.com>
2024-04-29 08:14:01 +0200

    ensure all MRG entries have a  `termType`
    Signed-off-by: Rieks <RieksJ@users.noreply.github.com>

--- mrgt/src/TuC.ts
+++ mrgt/src/TuC.ts
@@ -209,7 +209,8 @@ export class TuCBuilder {
         ctextYAML.navurl = navUrl.href
         ctextYAML.formPhrases = formPhrases
         ctextYAML.headingids = headingIds
-        ctextYAML.termid = `${ctextYAML.termType ?? this.saf.scope.defaulttype}:${ctextYAML.term}`
+        ctextYAML.termType = ctextYAML.termType || this.saf.scope.defaulttype || "concept";
+        ctextYAML.termid = `${ctextYAML.termType}:${ctextYAML.term}`
 
         if (ctextYAML.synonymOf) {
           TuCBuilder.synonymOf.push(ctextYAML)
